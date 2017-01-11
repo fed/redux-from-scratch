70d3db9c01967f76615d97adc7de390ed91b3c74
Federico Knüssel <fknussel@gmail.com>
2017-01-11 01:10:51 -0300

    Connect Loading container to the store

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {toggle as toggleLoading} from '../actions';
+import './Loading.css';
 
-const Loading = () => (
-  <div>Loading</div>
+const Loading = ({ loading, toggleLoading }) => (
+  <div>
+    <button onClick={toggleLoading}>Toggle Loading</button>
+    <p className={loading ? 'Loading-visible' : 'Loading-invisible'}>Loading...</p>
+  </div>
 );
 
-export default Loading;
+Loading.propTypes = {
+  loading: React.PropTypes.bool.isRequired,
+  toggleLoading: React.PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    loading: state.loading
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ toggleLoading }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Loading);
